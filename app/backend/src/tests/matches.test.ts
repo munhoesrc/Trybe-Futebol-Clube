b58894a4f2a7ea7eaf7cc1282efd35720da837e4
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-03-09 10:17:45 -0300

    feat: adicionando TDD para /matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -18,6 +18,8 @@ describe('Deve acessar o endpoint /matches', () => {
   sinon.restore();
 });
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoxLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsInVzZXJuYW1lIjoiQWRtaW4ifSwiaWF0IjoxNjc3NzYzODg0LCJleHAiOjE2Nzg2Mjc4ODR9.e5GVmFHZ9ZWiZ5Lc7kMNhXZjEu_DqnuMMwjRotatThY";
+
   const MatchesMoch = [
     new Matches({
       id: 1,
@@ -55,6 +57,14 @@ describe('Deve acessar o endpoint /matches', () => {
       inProgress: false
     }
   ]
+
+  const doingCreation = {
+    homeTeamId: 1,
+    homeTeamGoals: 1,
+    awayTeamId: 2,
+    awayTeamGoals: 2,
+    inProgress: true,
+  };
 
   it('Deve testar GET', async () => {
     sinon.stub(Model, 'findAll').resolves(MatchesMoch);
@@ -111,6 +121,40 @@ describe('Deve acessar o endpoint /matches', () => {
 
     expect(response).to.have.status(401);
     expect(response.body).to.deep.equal({ message: 'Token must be a valid token' });
+  });
+
+  it('Deve retornar, com um status 200, a seguinte mensagem: "Finished"', async () => {
+    const matchId = 1;
+    const url = `/matches/${matchId}/finish`;
+    const authToken = `${token}`;
+    const expectedResponse = { message: 'Finished' };
+
+    const updateStub = sinon.stub(Model, 'update').resolves();
+
+    const response = await chai
+    .request(app)
+    .patch(url)
+    .set({ Authorization: authToken });
+
+  expect(response.status).to.equal(200);
+  expect(response.body).to.deep.equal(expectedResponse);
+
+  updateStub.restore();
+  });
+
+  it('Deve acessar o create do matche de modo que seja possível cadastrar nova pardida', async () => {
+    const matchStub = sinon.stub(Model, 'create').resolves(MatchesMoch[1]);
+
+    const response = await chai
+      .request(app)
+      .post('/matches')
+      .send(doingCreation)
+      .set({ Authorization: `${ token }`});
+
+    expect(response.status).to.be.equal(201);
+    expect(response.body).to.deep.equal(MatchesMochControl[1]);
+
+    matchStub.restore();
   });
   
 });
